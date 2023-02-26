1498e13cf89d8ab51b14517deacdd1db27af1059
Monu kumar <120436973+7060monu@users.noreply.github.com>
2023-02-26 18:01:16 +0530

    Update AmountDistribution.c

--- AmountDistribution.c
+++ AmountDistribution.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 int main(){
-	int r,notes_500,notes_200,notes_100,notes_10;
+	int r,notes_500,notes_200,notes_100,notes_10,notes_50;
 	printf("enter amount:");
 	scanf("%d",&r);
 	notes_500=r/500;
 	notes_200=r%500/200;
 	notes_100=r%500%200/100;
+	notes_50=r%500%200%100/50;
 	notes_10=r%500%200%100/10;
 	printf("no of 500 notes =%d\n" ,notes_500);
 	printf("no of 200 notes = %d\n",notes_200);
 	printf("no of 100 notes = %d\n",notes_100);
+	printf("no of 50 notes = %d\n",notes_50);
 	printf("no of 10 notes = %d\n",notes_10);
 	return 0;
     
